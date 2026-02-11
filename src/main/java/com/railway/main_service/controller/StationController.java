77c7add5098407469e006797a584ff2c1af4c660
MEHULBANSAL2003 <mehulbansal83606@gmail.com>
2026-02-11 14:00:52 +0530

    minor changes

--- src/main/java/com/railway/main_service/controller/StationController.java
+++ src/main/java/com/railway/main_service/controller/StationController.java
@@ -15,6 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,7 +29,7 @@ public class StationController {
   private final StationServiceImpl stationService;
 
   @PostMapping(ApiConstants.ADD_NEW_STATION)
-  public ResponseEntity<ApiResponse<AddNewStationResponse>> addNewStation(AddNewStationRequest request) {
+  public ResponseEntity<ApiResponse<AddNewStationResponse>> addNewStation(@Valid @RequestBody AddNewStationRequest request) {
     AddNewStationResponse response = stationService.addNewStation(request);
 
     return ResponseEntity.ok(ApiResponse.success(response));
