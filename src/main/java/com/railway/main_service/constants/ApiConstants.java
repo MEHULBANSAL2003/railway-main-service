5fdd9a384bf9f0aea6349ef0095046b93c9ef307
MEHULBANSAL2003 <mehulbansal83606@gmail.com>
2026-03-05 12:19:54 +0530

    minor changes

--- src/main/java/com/railway/main_service/constants/ApiConstants.java
+++ src/main/java/com/railway/main_service/constants/ApiConstants.java
@@ -5,9 +5,7 @@ public class ApiConstants {
 
   public static final String STATIONS = API_BASE + "/stations";
 
-  public static final String TRAINS = API_BASE + "/trains";
 
-  public static final String ROUTES = API_BASE + "/routes";
   public static final String STATES = API_BASE + "/states";
   public static final String CITIES = API_BASE + "/cities";
   public static final String ZONES = API_BASE + "/zones";
@@ -15,7 +13,6 @@ public class ApiConstants {
   public static final String ADD_NEW_STATION = "/admin/add/new/station";
   public static final String UPLOAD_STATIONS_EXCEL = "/upload/excel";
   public static final String GET_STATIONS = "/get/all/list";
-  public static final String SEARCH_STATIONS = "/search/by/name";
   public static final String SET_ACTIVE_INACTIVE = "/set/active/inactive/{stationCode}";
   public static final String UPDATE_STATION_DETAILS = "/update/details/{stationCode}";
   public static final String DELETE_STATION = "/delete/{stationCode}";
@@ -33,7 +30,6 @@ public class ApiConstants {
   public static final String GET_CITIES = "/get/all";
   public static final String UPLOAD_CITIES_EXCEL = "/upload/excel";
   public static final String CITIES_BY_STATE_NAME = "/by/state/name";
-  public static final String GET_ALL_CITIES = "/get/all/list";
 
 
 
